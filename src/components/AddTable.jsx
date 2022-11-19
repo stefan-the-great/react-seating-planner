7f68498f4c454da75b552209c5de3a3ed0dbba87
grimreaper0112 <stefanfer01@gmail.com>
2022-11-20 05:10:16 +0530

    SVG Icons Refactored

--- src/components/AddTable.jsx
+++ src/components/AddTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDrop } from "react-dnd";
+import {ReactComponent as AddIcon} from '../images/Add.svg' 
 
 
 function AddTable({ setAllTables, nameList, setNameList}) {
@@ -29,10 +30,7 @@ function AddTable({ setAllTables, nameList, setNameList}) {
         style={{boxShadow: isOver ? "0px 0px 10px 0px rgba(0, 0, 0, 0.5)" : "0px 0px 0px rgba(0, 0, 0, 0)"}}>
         <div className="innerWrapper center">
             <div className='addTableBtn' onClick={addNewTable}>
-                <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 0h48v48H0z" fill="none"/>
-                    <path d="M8 12H4v28c0 2.21 1.79 4 4 4h28v-4H8V12zm32-8H16c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h24c2.21 0 4-1.79 4-4V8c0-2.21-1.79-4-4-4zm-2 18h-8v8h-4v-8h-8v-4h8v-8h4v8h8v4z" fill='#6e6e6e75'/>
-                </svg>
+                <AddIcon/>
             </div>
             <div className="addTableBtnLable">New Table</div>
             
