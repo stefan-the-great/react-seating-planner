94414961a7e177d0277c6bed689a488184d7cf39
stefanTheGreat <stefanfer01@gmail.com>
2025-06-05 15:17:37 +0530

    Final for Now

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import DragDrop from "./components/DragDrop.jsx"
 import Home from "./pages/Home.jsx";
+import AddNameList from "./pages/AddNameList/AddNameList.jsx";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Home/>}></Route>
+        <Route path={"add-names"} element={<AddNameList/>}></Route>
 
       </Routes>
       
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import './home.scss'
 import Column from '../components/Column/Column'
 import AddColumn from '../components/Column/AddColumn'
-import { DndContext, DragEndEvent } from '@dnd-kit/core'
+import { DndContext, DragEndEvent, useDroppable } from '@dnd-kit/core'
+import NameList from '../components/NameList/NameList'
+import FormatNameInput from '../components/utils/FormatNameInput'
+import TaskCard from '../components/TaskCard/TaskCard'
 
 const COLUMNS = [
     {id: 1, title: 1},
@@ -14,33 +17,25 @@ const INITTASKS = [
     {
         id: 1,
         name: "Stefan Fernando",
-        status: 1
+        status: 0
     },
     {
         id: 2,
-        name: "Stefan Fernando",
-        status: 1
-    },
-    {
-        id: 3,
-        name: "Stefan Fernando",
-        status: 2
-    },
-    {
-        id: 4,
-        name: "Stefan Fernando",
-        status: 2
+        name: "Josh",
+        status: 0
     },
 ]
 
 function Home() {
     const [tasks, setTasks] = useState(INITTASKS)
     const [columns, setColumns] = useState(COLUMNS)
+    const [nameInput, setNameInput] = useState('')
+    const [newIndex, setNewIndex] = useState(INITTASKS.length + 1)
 
     const handleAddCol = () => {
         setColumns((columns) => [...columns, {
-            id: columns.length,
-            title: columns.length
+            id: columns.length + 1,
+            title: columns.length + 1
         }])
     }
 
@@ -57,18 +52,56 @@ function Home() {
         } : task))
     }
 
+    const handleNameInputChange = (e) => {
+        setNameInput(e.target.value)
+    }
+
+    const handleNameInputSubmit = () => {
+        console.log(nameInput);
+    
+        setTasks((nameList) => ([...nameList, {id: newIndex, name: nameInput, status: 0}]));
+
+        setNewIndex(newIndex+1);
+
+    }
+
+    
+    const { setNodeRef } = useDroppable({
+    id: 0,
+    })
+
   return (
     <div className='home'>
         <div className="colContainer">
             <DndContext onDragEnd={handleDragEnd}>
+                {/* <NameList key={0} column={{id: 0, title: 0}} tasks={tasks.filter((task) => task.status === 0)} nameListLength={tasks.length} setTasks={setTasks} /> */}
 
-            {columns.map((column) => {
-                return <Column key={column.id} column={column} tasks={tasks.filter((task) => task.status === column.id)} />
-            })}
-            </DndContext>
-            <div className="addColWrapper" onClick={handleAddCol}>
-                <AddColumn/>
+                <div className='column' >
+                <h2 className='columnTitle'>
+                    {`Name List`}
+                </h2>
+                <div className="inputWrapper">
+                    <input type="text" className="nameInput" value={nameInput} onChange={handleNameInputChange} id="nameInput" />
+                    <div className="submitBtn" onClick={handleNameInputSubmit}>
+                        Add
+                    </div>
+                </div>
+                <div className="taskContainer">
+                <hr />
+                    {tasks.filter((task) => task.status === 0).map(task => {
+                    return <TaskCard key={task.id} task={task}/>;
+                    })}
+                </div>
             </div>
+
+
+                {columns.map((column) => {
+                    return <Column key={column.id} column={column} tasks={tasks.filter((task) => task.status === column.id)} />
+                })}
+                </DndContext>
+                <div className="addColWrapper" onClick={handleAddCol}>
+                    <AddColumn/>
+                </div>
         </div>
 
     </div>
