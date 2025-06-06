28228ef1a1507fbe6cec9a211b232ff7951fe632
stefanTheGreat <stefanfer01@gmail.com>
2025-06-06 13:16:32 +0530

    Further New Functions Added

---
+++ src/components/utils/ExportToCSV.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ExportToCSV(csv, filename) {
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
---
+++ src/components/utils/ExportToJSON.jsx
@@ -0,0 +1,12 @@
+export default function ExportToJSON(data, fileName) {
+    const jsonData = JSON.stringify(data, null, 2); // Use 2 for pretty formatting
+    const blob = new Blob([jsonData], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `${fileName}.json`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
